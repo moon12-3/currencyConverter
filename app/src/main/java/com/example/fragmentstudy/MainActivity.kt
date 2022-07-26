d78d714f74a0adb48e8b9acac77cac4547d8a84c
pickLotto <s2123@e-mirim.hs.kr>
2022-07-26 09:26:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/fragmentstudy/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.fragmentstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, CurrencyConverterFragment1())
+        transaction.commit()
+    }
+}
